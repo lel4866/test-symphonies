da8f5e5da0499ef341402f46fa2805c0e79237c7
Lawrence Lewis <lel486@gmail.com>
2026-01-05 08:06:50 -0500

    add Composer genrated csv data files

--- Test-SPY-QQQ_max-drawdown.clj
+++ Test-SPY-QQQ_max-drawdown.clj
@@ -1,29 +1,8 @@
 (defsymphony
- "Test-SPY-QQQ_max-drawdown"
+ "Test_SPY_QQQ_max_drawdown"
  {:asset-class "EQUITIES", :rebalance-frequency :daily}
- (weight-specified
-  0.6
-  (asset "SPY" "SSgA Active Trust - SPDR S&P 500 ETF Trust")
-  0.25
-  (asset
-   "QQQ"
-   "Invesco Capital Management LLC - Invesco QQQ Trust Series 1")
-  ##NaN
-  (if
-   (< (max-drawdown "TSLA" {:window 20}) 10)
-   [(asset "TSLA" "Tesla Inc")]
-   [(asset "NVDA" "NVIDIA Corp")])
-  0.15
-  (group
-   "Group1"
-   [(weight-equal
-     [(asset
-       "EDZ"
-       "Direxion Shares ETF Trust - Direxion Daily MSCI Emerging Markets Bear -3x Shares")
-      (asset "GLD" "SPDR Gold Trust - SPDR Gold Shares ETF")
-      (if
-       (> (rsi "TSLA" {:window 14}) 60)
-       [(asset "NVDA" "NVIDIA Corp")]
-       [(asset
-         "GOOGL" 
-         "Alphabet Inc - Ordinary Shares - Class A")])])])))
+ (weight-equal
+  [(if
+    (< (max-drawdown "SPY" {:window 20}) 5)
+    [(asset "SPY" "SSgA Active Trust - SPDR S&P 500 ETF Trust")]
+    [(asset "TSLA" "Tesla Inc")])]))
